a4222ce6bb923f918bb025f35dcdee9c021fc8c5
Nauman-Arshad <naumankhan642@gmail.com>
2021-09-05 22:27:41 +0500

    if else add on  signs  options

---
+++ db/migrate/20210905114512_add_columns_to_user.rb
@@ -0,0 +1,7 @@
+class AddColumnsToUser < ActiveRecord::Migration[6.1]
+  def change
+    add_column :users, :admin, :boolean, default: false
+    add_column :users, :firstname, :string
+    add_column :users, :lastname, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_04_134547) do
+ActiveRecord::Schema.define(version: 2021_09_05_114512) do
 
   create_table "users", force: :cascade do |t|
     t.string "username", default: "", null: false
@@ -25,6 +25,9 @@ ActiveRecord::Schema.define(version: 2021_09_04_134547) do
     t.string "unconfirmed_email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "admin", default: false
+    t.string "firstname"
+    t.string "lastname"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
