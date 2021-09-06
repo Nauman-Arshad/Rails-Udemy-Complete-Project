38c1a91fe69805fd21af7998a879c2b5a843305d
Nauman-Arshad <naumankhan642@gmail.com>
2021-09-06 15:51:03 +0500

    add Smtp info

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,10 +1,10 @@
 ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
-  :address => 'smtp.sendgrid.net',
+  :address => 'smtp.gmail.com',
   :port => '587',
-  :authentication => :plain,
-  :user_name => ENV["SENDGRID_USERNAME"],
-  :password => ENV["SENDGRID_PASSWORD"],
+  :authentication => :yes,
+  :user_name => ENV["naumankhan642@gmail.com"],
+  :password => ENV["naumankhan642"],
   :domain => 'heroku.com',
   :enable_starttls_auto => true
 
